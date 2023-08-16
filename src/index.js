ddd468cd225fdf3316df8b041ac3b45f74f3bd23
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-16 10:49:51 -0700

    dummy lists and item lists up + date-fns import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+import { compareAsc, format } from 'date-fns'
+
 const ProjectsArray = []
 const ListArray = []
 const ListItemsArray = []
+
+function addProject(project) {
+    ProjectsArray.push(project)
+}
+
+function addList(list) {
+    ListArray.push(list)
+}
+
+function addListItem(item) {
+    ListItemsArray.push(item)
+} 
 
 class Projects {
     constructor (projectName, list) {
@@ -10,7 +24,7 @@ class Projects {
 }
 
 class Lists {
-    constructor (listName, listItemsArray) {
+    constructor (listName, listItems) {
        this.listName = listName
        this.listItems = listItems
 
@@ -23,6 +37,13 @@ class ListItems {
     }
 }
 
+const listItemOne = new ListItems("itemOne", "does stuff", "1/1/2024", "low", false)
+const listItemTwo = new ListItems("itemTwo", "does more stuff", "5/5/2054", "medium", true)
+const listItemThree = new ListItems("itemThree", "does so much stuff", "3/3/2034", "high", false)
+
+const listOne = new Lists("test list", [listItemOne, listItemTwo, listItemThree])
+
+const projectOne = new Projects("Test Project", listOne)
 
 
-console.log(new Projects("Test Project", ["item one", "item two", "item three"]))
+console.log(projectOne)
