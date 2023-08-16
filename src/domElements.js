d4b49358f1b3474496f96786afa144942b8455c8
Brian Salomone <brian.j.salomone@gmail.com>
2023-08-16 11:46:46 -0700

    added 'add' button + bg

--- src/domElements.js
+++ src/domElements.js
@@ -2,11 +2,20 @@ const content = document.querySelector(".content")
 
 export function createDomElements() {
     content.appendChild(projectHeader("Add a project!"))
+    content.appendChild(addProjectButton())
 }
 
-function projectHeader(title) {
+const projectHeader = (title) => {
     const div = document.createElement("div")
     div.classList.add("add-project-header")
     div.innerText = title
     return div
+}
+
+const addProjectButton = () => {
+    const button = document.createElement("button")
+    button.innerHTML = "<img src='../src/images/plus-circle.svg'/>"
+    button.style.width = "100px"
+    button.style.backgroundColor = "white"
+    return button
 }
